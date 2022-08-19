70e66d6edebeadeb41ebf13aa627762cd3a6cd63
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2022-08-19 11:39:53 -0500

    Create extract_hidden_images.py

---
+++ extract_hidden_images.py
@@ -0,0 +1,11 @@
+#----------------------------------------------------------
+# Lab #1: Steganography
+# Image processing through bit manipulation.
+#
+# Date: 26-Aug-2022
+# Authors:
+#           A01745446 Sergio Manuel Gonzalez Vargas
+#           A013 Rodrigo Alfredo Mendoza España
+#----------------------------------------------------------
+
+from PIL import Image
