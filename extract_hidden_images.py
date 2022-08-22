d4d90644eb42b16ca2801b28fd48c4b4d1c336f8
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-08-22 14:50:28 -0500

    Parte 2 casi lista

--- extract_hidden_images.py
+++ extract_hidden_images.py
@@ -30,38 +30,84 @@ import string
 from PIL import Image
 import sys
 
+
+# def extract_hidden_images(image: Image.Image) -> None:
+#     pixin = image.load()  # type: ignore
+#     width, height = image.size
+    
+#     output_image = Image.new('1', (width, height))
+#     pixout = output_image.load()  # type: ignore
+        
+#     for element in range(1,4):
+#         for y in range(height):
+#             for x in range(width):
+#                 MULTIPLE_COLOR_LIST[element]= pixin[x, y]
+                
+#                 if COLOR_LIST[element] & 1:
+#                     pixout[x, y] = 1  # black
+                    
+#                 else:
+#                     pixout[x, y] = 0  # white
+
+#         output_image.save(OUT_LIST[element])
+
 def process_image(file_name: str) -> None:
     if ".png" in file_name:
-        with Image.open(file_name) as image:
-            if image.mode == "RGB":
-                image.show()
-            else:
-                print("The mode of the file is not RGB.")
-                sys.exit()
+        image = Image.open(file_name)
+        if image.mode == "RGB":
+            # extract_hidden_images(image)
+            
+            
+            pixin = image.load()  # type: ignore
+            width, height = image.size
+        
+            output_image = Image.new('1', (width, height))
+            pixout = output_image.load()  # type: ignore
+            
+            for y in range(height):
+                for x in range(width):
+                    r,_,_ = pixin[x, y]
+                    if r & 1:
+                        pixout[x, y] = 1  # black
+                        
+                    else:
+                        pixout[x, y] = 0  # white
+            output_image.save("scarlett_chanel_1_red.png")
+
+      
+            for y in range(height):
+                for x in range(width):
+                    _,g,_ = pixin[x, y]
+                    if g & 1:
+                        pixout[x, y] = 1  # black
+                        
+                    else:
+                        pixout[x, y] = 0  # white
+
+            output_image.save("scarlett_chanel_2_green.png")
+            
+            
+            for y in range(height):
+                for x in range(width):
+                    _,_,b = pixin[x, y]
+                    if b & 1:
+                        pixout[x, y] = 1  # black
+                        
+                    else:
+                        pixout[x, y] = 0  # white
+
+            output_image.save("scarlett_chanel_3_blue.png")
+            
+        else:
+            print("The mode of the file is not RGB.")
+            sys.exit()
     else:
         print("Error: The provided file name doesn't have a .png extension.")
         sys.exit()
     
-    # with Image.open(INPUT_FILE_NAME) as input_file:
-    #     pixin = input_file.load() # type: ignore
-    #     width, height = input_file.size
-        
-    # output_image = Image.new('RGB', (width, height))
-    # pixout = output_image.load() # type: ignore
-    
-    # for y in range(height):
-    #     for x in range(width):
-    #         r, g, b = pixin[x, y]
-    #         pixout[x, y] = (r, 0, 0)
-    
-    # output_image.save(OUTPUT_FILE_NAME)
-    
-    
-
-
 if '__main__' == __name__:
-    # INPUT_FILE_NAME = sys.argv[1] # Leer el nombre del archivo de entrada desde la terminal
+    INPUT_FILE_NAME = sys.argv[1] # Leer el nombre del archivo de entrada desde la terminal
     # INPUT_FILE_NAME = "scarlett.jpg"
-    INPUT_FILE_NAME = "scarlett.png"
+    # INPUT_FILE_NAME = "scarlett.png" 
     
     process_image(INPUT_FILE_NAME)
