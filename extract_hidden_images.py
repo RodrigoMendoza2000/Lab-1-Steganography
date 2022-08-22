41417f824e58119ed8a8fc219eb2ce9edf0172f5
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-08-22 13:57:22 -0500

    PASO 1

--- extract_hidden_images.py
+++ extract_hidden_images.py
@@ -8,12 +8,39 @@
 #           A013 Rodrigo Alfredo Mendoza España
 #----------------------------------------------------------
 
+# CHECK LIST
+
+# 1-. Takes as a command line argument the name of an RGB mode PNG file. 
+#   he program should print an error message and quit under the following circumstances:
+#
+# - The name of the file was not provided as a command line argument. OKAY
+# - The provided file name doesn’t have a .png extension. OKAY
+# - An exception is raised when trying to open the image file. It’s important to specify 
+#   the reason as part of the error message. OKAY
+# - The mode of the file is not RGB. OKAY
+#
+# 2-. Extracts from the red, green, and blue channels the corresponding hidden 1-bit images placing the result in three 1-bit 
+#   PNG images with the following suffixes after the original extensionless file-name :
+#
+# - file-name_channel_1_red.png
+# - file-name_channel_2_green.png
+# - file-name_channel_3_blue.png
+
 import string
 from PIL import Image
 import sys
 
 def process_image(file_name: str) -> None:
-    print(file_name) # Borrar para entregar
+    if ".png" in file_name:
+        with Image.open(file_name) as image:
+            if image.mode == "RGB":
+                image.show()
+            else:
+                print("The mode of the file is not RGB.")
+                sys.exit()
+    else:
+        print("Error: The provided file name doesn't have a .png extension.")
+        sys.exit()
     
     # with Image.open(INPUT_FILE_NAME) as input_file:
     #     pixin = input_file.load() # type: ignore
@@ -28,8 +55,13 @@ def process_image(file_name: str) -> None:
     #         pixout[x, y] = (r, 0, 0)
     
     # output_image.save(OUTPUT_FILE_NAME)
+    
+    
 
 
 if '__main__' == __name__:
-    INPUT_FILE_NAME = sys.argv[1] # Leer el nombre del archivo de entrada desde la terminal
+    # INPUT_FILE_NAME = sys.argv[1] # Leer el nombre del archivo de entrada desde la terminal
+    # INPUT_FILE_NAME = "scarlett.jpg"
+    INPUT_FILE_NAME = "scarlett.png"
+    
     process_image(INPUT_FILE_NAME)
